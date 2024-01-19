3c8045d2ca8d42fa1940bec35809d305945ba920
jabilisunal <jabilisunal@gmail.com>
2024-01-19 15:57:38 +0400

    inital commit

---
+++ assets/js/script.js
@@ -0,0 +1,179 @@
+"use strict";
+//console.log("Hello World");
+
+// a=5;
+// b=3;
+// c=10;
+
+// d=(a+b)-c;
+// console.log(d)
+
+// console.log("----------------")
+// console.log(a+b);
+// console.log(c-b);
+// console.log(a*b)
+// console.log(c/a);
+
+// ! JS Variables
+// var
+// let
+// const
+
+// ? JS DataTypes
+
+//? Number
+// let a = 10;
+// let number = 15;
+// let reqem = 34;
+// let eded = number-reqem
+// let pi = 3.14;
+// let minus = -30;
+
+// console.log(eded)
+// console.log(a);
+// console.log(number - pi);
+// console.log(pi);
+
+
+// console.log("------- strings start ------");
+// ? String
+
+// let name = "Sunal";
+// let surname = "Jabili";
+// let university = "ASOIU";
+// // let isCheck ="false";
+
+// let age =24;
+
+// console.log(name);
+// console.log(surname);
+// console.log(university);
+
+// console.log(name,surname,university,age)
+
+// ? typeof data tiplerini mueyyen edir.
+
+// console.log(typeof name);
+// console.log(typeof age)
+
+
+// ? Boolean
+
+// let isMarried = false;
+// let isCheckUniversity =true;
+// let isIll = false;
+
+
+// console.log(typeof isCheckUniversity);
+// console.log(typeof isIll);
+
+// ? Undefined
+
+// let x;
+// let fullName;
+// console.log(x);
+// console.log(fullName)
+
+// ? Null
+
+// ? BigInt
+// interger
+// int
+// number
+// bigInt
+
+// let number1 =12312n;
+// let number2 =12311n;
+// let y = BigInt("12341241212");
+// console.log(typeof number1)
+// console.log(number1-number2);
+// console.log(typeof y)
+
+//? Symbol JS
+
+// let example =Symbol("example");
+// console.log(typeof example)
+
+
+// ? Operators
+
+//! Arithmetic Operators
+
+// let x =10;
+// let y=5;
+
+// console.log(x+y); //15
+// console.log(x-y); //5
+// console.log(x/y);//2
+// console.log(x*y); //50
+
+// let a =2;
+// let b =3
+// ** bu ishare quvvet yukseletmedir.
+// console.log(a**b); //2*2*2
+// console.log(3**3)
+
+// ? ++ -- 
+// let number =10;
+
+// console.log(number);
+
+
+// console.log(number++); //11
+// console.log(number)
+
+// let z =5;
+// z++;
+// console.log(z)
+// console.log(++a);
+
+//? % -mod qaliq qaytarir.
+
+
+// let x = 20;
+
+// console.log(5%6);
+// ? Assigment Operators
+// let x =10;
+// x+=5;
+// x=54;
+// x+=10   // x**10 =1000000
+
+// console.log(x);
+
+
+
+// ! Comprasion Operators
+
+// let number1 =10;
+// let number2 ="10";
+
+// console.log(number1==number2) // 10 =="10"  true
+// console.log(number1===number2); // 10 ==="10" false datatyplerinda yoxlayir.
+// console.log(number1===number2)
+
+
+// let isMarried =true;
+// console.log(!isMarried)
+
+let x =10;
+let y =10;
+
+// console.log(x==y);
+// console.log(x!=y);
+
+// console.log(x!==y); // true
+
+// console.log(x>y);
+
+// console.log(x<y);
+
+// console.log(x>=y);
+console.log(x<=y)
+
+
+
+
+
+
+
